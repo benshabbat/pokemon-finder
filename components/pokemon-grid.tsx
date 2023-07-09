e3da453d60c21e4e1884fd15a9453a06a4d3d227
benshabbat <benshabbat27@gmail.com>
2023-07-09 17:25:58 +0300

    start pokemon grid

--- components/pokemon-grid.tsx
+++ components/pokemon-grid.tsx
@@ -1,7 +1,17 @@
-"use client"
+"use client";
 
-import { useState } from "react"
+import { useState } from "react";
 
-export function PokemonGrid(){
-const [searchText, setSearchText] = useState()
+export function PokemonGrid() {
+  const [searchText, setSearchText] = useState();
+  return (
+    <>
+      <div>
+        <h3 className="text-2xl py-6 text-center">Search For Your Pokemon!</h3>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+
+        </div>
+      </div>
+    </>
+  );
 }
