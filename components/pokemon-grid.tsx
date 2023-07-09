39b9f3b958bef4eb59e1bd1ac2155e4c6bd990e4
benshabbat <benshabbat27@gmail.com>
2023-07-09 18:48:20 +0300

    create pokemin grid with label and input search

--- components/pokemon-grid.tsx
+++ components/pokemon-grid.tsx
@@ -1,15 +1,28 @@
 "use client";
 
 import { useState } from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
 export function PokemonGrid() {
   const [searchText, setSearchText] = useState();
+  const searchPokemon = (e:any) => {
+    const { value } = e.target;
+    setSearchText(value);
+  };
   return (
     <>
       <div>
         <h3 className="text-2xl py-6 text-center">Search For Your Pokemon!</h3>
         <div className="grid w-full max-w-sm items-center gap-1.5">
-
+          <Label htmlFor="pokemonName">Pokemon Name</Label>
+          <Input
+            type="text"
+            id="pokemonName"
+            placeholder="Pokemon Name"
+            value={searchText}
+            onChange={searchPokemon}
+          />
         </div>
       </div>
     </>
