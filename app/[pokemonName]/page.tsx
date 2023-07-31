fd7f4b7e497f6ce8c36fe522d311909ada7bad82
benshabbat <benshabbat27@gmail.com>
2023-07-31 11:43:08 +0300

    fix ui image pokemon

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -13,7 +13,7 @@ export default async function PokemonPage({
       <h1 className="text-4xl text-bold pt-4">
         {pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}
       </h1>
-      <div className="mb-4">
+      <div className="m-4" style={{position:"relative", width:"300px", height:"300px"}}>
         <PokemonImage
           image={pokemonObject.sprites.other["official-artwork"].front_default}
           name={pokemonName}
