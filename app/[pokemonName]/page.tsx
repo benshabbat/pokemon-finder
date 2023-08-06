94848234f5ccd637c4bb2b8a5721ff3e19df02a5
benshabbat <benshabbat27@gmail.com>
2023-08-06 22:59:37 +0300

    add stats for pokemon

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -13,11 +13,31 @@ export default async function PokemonPage({
       <h1 className="text-4xl text-bold pt-4">
         {pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}
       </h1>
-      <div className="m-4" style={{position:"relative", width:"300px", height:"300px"}}>
+      <div
+        className="m-4"
+        style={{ position: "relative", width: "300px", height: "300px" }}
+      >
         <PokemonImage
           image={pokemonObject.sprites.other["official-artwork"].front_default}
           name={pokemonName}
         />
+      </div>
+      <h3>Weight:{pokemonObject?.weight}</h3>
+      <h3>Height:{pokemonObject?.height}</h3>
+      <div className="flex-col">
+        {pokemonObject?.stats?.map((statObject: any) => {
+          const statName = statObject?.stat?.name;
+          const statValue = statObject?.base_stat;
+          return (
+            <div
+              className="flex items-stretch"
+              style={{ width: "500px" }}
+              key={statName}
+            >
+              <h3 className="p-3 w-2/4">{statName}:{statValue}</h3>
+            </div>
+          );
+        })}
       </div>
     </>
   );
